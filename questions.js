501396339c3c5d3fc69aba19d3336b0deaff65fd
ArchitTiwari-max <118987906+ArchitTiwari-max@users.noreply.github.com>
2023-10-23 12:10:33 +0530

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,49 +1,48 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
+  {
+    numb: 1,
+    question: "The correct sequence of HTML tags for starting a webpage is -",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Head, Title, Body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body"
+    ]
+  },
     {
-      numb: 1,
-      question: "Which of the following attributes is used to add link to any element?",
-      answer: "href",
-      options: [
-        "link",
-        "href",
-        "ref",
-        "newref"
-      ]
-    },
-      {
-      numb: 2,
-      question: "What does HTML stand for?",
-      answer: "Hyper Text Markup Language",
-      options: [
-        "Hyper Text Markup Language",
-        "Hyper Text Preprocessor",
-        "Hyper Text Multiple Language",
-        "Hyper Tool Multi Language"
-      ]
-    },
+    numb: 2,
+    question: "  Which of the following attribute is used to provide a unique name to an element?",
+    answer: "id",
+    options: [
+      "id",
+      "class",
+      "type",
+      "none of the above"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What are the types of unordered or bulleted list in HTML?",
+    answer: "disc, circle, square",
+    options: [
+      "disc, circle, square",
+      "disc, square, triangle",
+      "polygon, triangle, circle",
+      "all of the above"
+    ]
+  },
     {
-      numb: 3,
-      question: "What does PHP stand for?",
-      answer: "Hypertext Preprocessor",
-      options: [
-        "Hypertext Preprocessor",
-        "Hypertext Programming",
-        "Hypertext Preprogramming",
-        "Hometext Preprocessor"
-      ]
-    },
-      {
-      numb: 4,
-      question: "What does CSS stand for?",
-      answer: "Cascading Style Sheet",
-      options: [
-        "Common Style Sheet",
-        "Colorful Style Sheet",
-        "Computer Style Sheet",
-        "Cascading Style Sheet"
-      ]
-    }
-      
-  ];
-  
+    numb: 4,
+    question: "A program in HTML can be rendered and read by -",
+    answer: "Web browser",
+    options: [
+      "Server",
+      "Interpreter",
+      "Both a and b",
+      "Web browser"
+    ]
+   }
+      
+  ];
